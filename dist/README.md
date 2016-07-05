b98d6408dab62b89117f4f0d62aac3d9e3a413d4
Christian <christian.straubhaar@gmail.com>
2016-07-05 18:05:56 -0500

    finished basic

---
+++ dist/README.md
@@ -0,0 +1,10 @@
+# Assignment: Build Etsy Search Page using Ajax
+
+### Special things needed
+1. etsy requires your ajax request settings to have the `dataType: jsonp`
+2. surge url to use https://jcsjtiyday16etsysearch.surge.sh/
+3. Etsy API
+
+
+## Issues
+1. Tried to get the search bar functionality going but wasn't able to get the whole thing to rerun on return key keyup. I tried wrapping it all in a function that I'd call on return keyup, but it didn't render anything. So, I'll have to ask Jess tomorrow how to get that kind of thing to work.
--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,56 @@
-var resultHTML = '<section class="result"><img src=""/><div class="label"><p class="title"></p><p href="#" class="seller"></p><p class="price"></p></div></section>';
+var resultHTML = '<a class="result" href=""><img src=""/><div class="label"><p class="title"></p><p class="seller"></p><p class="price"></p></div></a>';
+
 var $container = $('.container');
+var $header = $('header');
+var $searchBar = $('input');
+
+var url1 = 'https://api.etsy.com/v2/listings/active.js?api_key=5j9tq3jewb2f9h9zlk988uzz&keywords=';
+var searchTerm = 'swant';
+var url2 = '&includes=Images,Shop';
+
+// function search() {
+// if (searchTerm) {
 
 var settings = {
-  url: 'https://api.etsy.com/v2/listings/active.js?api_key=5j9tq3jewb2f9h9zlk988uzz&keywords=swant&includes=Images,Shop',
+  url: (url1+searchTerm+url2),
   type: 'GET',
   success: function renderResults(response) {
 
+    // var urlSplit = settings.url.split('=');
+    // var searchTermArray = urlSplit[2].split('&');
+    // var searchTerm = searchTermArray[0];
+    $searchBar.attr('value', searchTerm);
+
+    $('.num-results').text('"'+searchTerm+'" ('+response.count+' results)');
+
     response.results.forEach(function(result, i, responseArr){
+
       var $result = $(resultHTML);
+
+      $result.attr('href', result.url);
+
       $result.children('img').attr('src', result.Images[0].url_170x135);
+
+      $result.find('.title').text(result.title);
+      $result.find('.seller').text(result.Shop.shop_name);
+
       $container.append($result);
+
     });
   },
   dataType: 'jsonp',
 };
 
+
+
 $.ajax(settings);
+
+// }
+// }
+
+// $searchBar.keyup(function (evt) {
+//   if(evt.keyCode == 13){
+//     searchTerm = $searchBar.attr('value');
+//     search();
+//   }
+// });
